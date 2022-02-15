9217209191b837437fccca1c6f5188c28dc4a34a
Your Name <you@example.com>
2022-02-15 18:26:45 +0900

    createに繋がるふよふよボタンの型ができた

--- top/top.js
+++ top/top.js
@@ -2,3 +2,24 @@
 $('.nav_toggle').on('click', function() {
     $('.nav_toggle, .nav').toggleClass('show');
 });
+
+$(function() {
+    // 変数にクラスを入れる
+    var btn = $('.button');
+
+    //スクロールしてページトップから100に達したらボタンを表示
+    $(window).on('load scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            btn.addClass('active');
+        } else {
+            btn.removeClass('active');
+        }
+    });
+
+    //スクロールしてトップへ戻る
+    btn.on('click', function() {
+        $('body,html').animate({
+            scrollTop: 0
+        });
+    });
+});
